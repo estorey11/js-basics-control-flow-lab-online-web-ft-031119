50ac4682c2355dd1cc8442f279e7b5a2ac9ffcbe
Eric Storey <estorey@gmail.com>
2019-06-14 20:04:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function scuberGreetingForFeet(feet){
   // Write your code here!
   if (feet < 400){
-    return 'This one is on me!'
+    return 'This one is on me!';
   } elsif (feet>2000){
-    return 'I will gladly take your thirty bucks.'
+    return 'I will gladly take your thirty bucks.';
   } elsif (feet>2500){
-    return 'No can do.'
+    return 'No can do.';
   }
 }
 

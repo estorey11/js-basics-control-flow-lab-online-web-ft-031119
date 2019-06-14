978f00c33a59a645e9ac8870ad72989e90a60d7a
Eric Storey <estorey@gmail.com>
2019-06-14 20:09:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function scuberGreetingForFeet(feet){
 
 function ternaryCheckCity(city){
   // Write your code here!
-  city === "NYC" ? return "Ok, sounds good." : return 
+  city === "NYC" ? return "Ok, sounds good." : return "No go.";
 }
 
 function switchOnCharmFromTip(){
